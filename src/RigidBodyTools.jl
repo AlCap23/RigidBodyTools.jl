be30b7f36b8a7df948b83df4a743e13fc63724b7
AlCap23 <julius.martensen@dfki.de>
2020-04-21 08:29:40 +0200

    Files generated by PkgTemplates

---
+++ src/RigidBodyTools.jl
@@ -0,0 +1,5 @@
+module RigidBodyTools
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RigidBodyTools
+using Test
+
+@testset "RigidBodyTools.jl" begin
+    # Write your own tests here.
+end
